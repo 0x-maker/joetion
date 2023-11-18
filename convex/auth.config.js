685da17d9827a4fa5dde3a003a9161bb3928f055
Ali Jorani <52755848+ajorani92@users.noreply.github.com>
2023-11-18 17:57:59 -0600

    joetion

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://desired-swift-0.clerk.accounts.dev",
+      applicationID: "convex-0x-projects",
+    }
+  ]
+}
